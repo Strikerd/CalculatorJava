2b9d38999001854fe55649fef55aa9610fce5d30
Strikerd <imahach@mail.ru>
2021-02-21 20:56:58 +0300

    CommitFirst

---
+++ src/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String [] args) {
+
+        Scanner console = new Scanner(System.in);
+        int a = console.nextInt();
+        int b = console.nextInt();
+
+        System.out.println(a + b);
+    }
+}
